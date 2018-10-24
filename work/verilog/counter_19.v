d49833375b948e31a7753d46bbcc6983c5f396cf
teezhiyao <teezhiyao@gmail.com>
2018-10-24 15:24:36 +0800

    Revert "first commit"
    This reverts commit 65de869f4b1e311f97e6893dd5b8622a3bfc9489.

--- work/verilog/counter_19.v
+++ work/verilog/counter_19.v
@@ -6,40 +6,40 @@
 
 /*
    Parameters:
-     SIZE = 1
-     DIV = 24
-     TOP = 0
+     SIZE = DIGIT_BITS
+     DIV = DIV
+     TOP = DIGITS-1
      UP = 1
 */
-module counter_12 (
+module counter_19 (
     input clk,
     input rst,
-    output reg [0:0] value
+    output reg [1:0] value
   );
   
-  localparam SIZE = 1'h1;
-  localparam DIV = 5'h18;
-  localparam TOP = 1'h0;
+  localparam SIZE = 2'h2;
+  localparam DIV = 5'h10;
+  localparam TOP = 4'h3;
   localparam UP = 1'h1;
   
   
-  reg [24:0] M_ctr_d, M_ctr_q = 1'h0;
+  reg [17:0] M_ctr_d, M_ctr_q = 1'h0;
   
-  localparam MAX_VALUE = 25'h0ffffff;
+  localparam MAX_VALUE = 20'h3ffff;
   
   always @* begin
     M_ctr_d = M_ctr_q;
     
-    value = M_ctr_q[24+0-:1];
+    value = M_ctr_q[16+1-:2];
     if (1'h1) begin
       M_ctr_d = M_ctr_q + 1'h1;
-      if (1'h0 && M_ctr_q == 25'h0ffffff) begin
+      if (1'h1 && M_ctr_q == 20'h3ffff) begin
         M_ctr_d = 1'h0;
       end
     end else begin
       M_ctr_d = M_ctr_q - 1'h1;
-      if (1'h0 && M_ctr_q == 1'h0) begin
-        M_ctr_d = 25'h0ffffff;
+      if (1'h1 && M_ctr_q == 1'h0) begin
+        M_ctr_d = 20'h3ffff;
       end
     end
   end
